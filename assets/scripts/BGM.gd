b0d5a5f3c55b1ba3bc0f68de11fc4cc74d5abfc6
orangeoceans <90552466+orangeoceans@users.noreply.github.com>
2023-10-02 04:34:28 -0700

    Add fading bgm

---
+++ assets/scripts/BGM.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+var muted = false
+var master_volume
+var current_stream
+var dialogue_system
+var min_volume = -80.
+var audible_dist = 7.
+
+var keypoint_streams = {
+	"statue": $lofi,
+	"trumpet": $trumpet,
+	"salt": $chime
+}
+
+func linear_to_db(linear):
+	if linear <= 0:
+		return -80.0 # a very low value to represent silence
+	else:
+		return clamp(20 * log(linear) / log(10), -80.0, 0.)
+
+var prev_dist = 999999
+func update_audio():
+	var key = dialogue_system.nearest_encounter
+	var dist = dialogue_system.nearest_encounter_dist
+	if dist != prev_dist and dist <= audible_dist and key in keypoint_streams:
+		current_stream = keypoint_streams[key]
+		if not current_stream.playing:
+			current_stream.playing = true
+		var normalized_dist = dist/audible_dist
+	
+		$ambience.volume_db = linear_to_db(normalized_dist)
+		current_stream.volume_db = linear_to_db(1. - normalized_dist)
+		prev_dist = dist
+	elif dist != prev_dist:
+		if current_stream:
+			current_stream.playing = false
+			current_stream = null
+		$ambience.volume_db = master_volume
+	
+
+func start_audio():
+	$ambience.play()
+
+func _ready():
+	master_volume = -20.
+	dialogue_system = get_node("/root/square_3D/DialogueSystem")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	update_audio()
+	print(master_volume)
+	print($ambience.volume_db)
--- assets/scripts/IntroScreenController.gd
+++ assets/scripts/IntroScreenController.gd
@@ -4,12 +4,14 @@ var root_node: Node3D
 var intro_transition: TextureRect
 var dialogue_button: Button
 var camera: Camera3D
+var BGM: Node2D
 
 func _ready():
 	root_node = get_node("/root/square_3D")
 	intro_transition = get_node("../IntroTransition")
 	dialogue_button = get_node("/root/square_3D/DialogueSystem/InteractButton")
 	camera = get_node("/root/square_3D/Camera3D")
+	BGM = get_node("/root/square_3D/BGM")
 
 func _process(_delta):
 	pass
@@ -19,6 +21,9 @@ func _on_start_button_pressed():
 	tween.tween_property(intro_transition, "modulate", Color(1.,1.,1.,1.), 0.6)
 	tween.tween_property(self, "modulate", Color(0,0,0,0), 0.2)
 	tween.tween_property(intro_transition, "modulate", Color(1.,1.,1.,0.), 1.)
+	BGM.start_audio()
+	var audio_tween = get_tree().create_tween()
+	audio_tween.tween_property(BGM, "master_volume", 0, 0.8)
 	await get_tree().create_timer(0.8).timeout
 	var camera_tween = get_tree().create_tween().set_parallel(true)
 	camera_tween.tween_property(camera, 
